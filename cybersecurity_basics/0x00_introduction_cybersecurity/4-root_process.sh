702cca2b35870170f2cc1f0b21b16e2a87a6a32a
rebiw05 <ismayilovarabia5@gmail.com>
2025-08-06 07:05:05 -0400

    exclude vsz and rss

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd \
-    | grep -v "VSZ" \
-    | grep -v " 0 0 "
+ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd | grep -v "VSZ" | grep -v " 0 0 "
