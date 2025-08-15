a3ee7b9eaa126f1fa679d7a5984f8b2841897417
rebiw05 <ismayilovarabia5@gmail.com>
2025-08-15 19:46:07 -0400

    whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+domain=$1
+
+whois "$domain" | awk -F: '
+/^Registrant|^Admin|^Tech/ {
+    key=$1
+    value=$2
+    gsub(/^[ \t]+/, "", value)   # başlanğıc boşluqları sil
+    gsub(/[ \t]+$/, "", value)   # sondakı boşluqları sil
+    print key "," value
+}' > "$domain.csv"
