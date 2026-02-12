9a9aea94b207258ecccee67030e99f3d1a5d0044
rebiw05 <ismayilovarabia5@gmail.com>
2026-02-11 22:27:43 -0500

    sorting urls

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $7}' logs.txt | sort | uniq -c | sort -nr | awk '{print $2}' | head -1
+awk -F'"' '{print $2}' logs.txt | awk '{print $2}' | sort | uniq -c | sort -nr | awk '{print $2}' | head -1
